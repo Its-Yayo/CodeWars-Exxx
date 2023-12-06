86f13cf1b60835ef4735cf11bb22af8c117de71b
Its-Yayo <a01754574@itesm.mx>
2023-12-06 11:09:29 -0600

    is_triangle.py: usages

--- src/Easy/is_triangle.py
+++ src/Easy/is_triangle.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""
+Instructions:
+Source: https://www.codewars.com/kata/56606694ec01347ce800001b
+
+Implement a function that accepts 3 integer values a, b, c.
+The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+"""
+
+def is_triangle(a, b, c):
+    # Using the triangle inequality problem
+    return True if a + b > c and a + c > b and b + c > a else False
