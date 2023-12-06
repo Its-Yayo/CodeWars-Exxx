07754fbac3de9066d885de17d029808d0e2f6b9f
Its-Yayo <a01754574@itesm.mx>
2023-12-06 11:06:36 -0600

    odd_or_even.py: usages

--- src/Easy/odd_or_even.py
+++ src/Easy/odd_or_even.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+"""
+Instructions:
+Source: https://www.codewars.com/kata/5949481f86420f59480000e7
+
+Given a list of integers, determine whether the sum of its elements is odd or even.
+Give your answer as a string matching "odd" or "even".
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+    Input: [0]
+    Output: "even"
+
+    Input: [0, 1, 4]
+    Output: "odd"
+
+    Input: [0, -1, -5]
+    Output: "even"
+
+"""
+
+def odd_or_even(arr: list) -> str:
+    sum_numbers = sum(arr)
+    return "odd" if sum_numbers % 2 == 1 else "even"
