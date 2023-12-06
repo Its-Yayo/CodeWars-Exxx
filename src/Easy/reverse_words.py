7a2f28c827a231de85dab03fd5f29d1225d78aff
Its-Yayo <a01754574@itesm.mx>
2023-12-06 11:14:13 -0600

    reverse_words.py: usages

--- src/Easy/reverse_words.py
+++ src/Easy/reverse_words.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+"""
+Instructions:
+Source: https://www.codewars.com/kata/5259b20d6021e9e14c0010d4
+
+Complete the function that accepts a string parameter, and reverses each word in the string.
+All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+
+"""
+
+
+def reverse_words(text):
+    words = text.split(' ')
+    reversed_words = []
+
+    for word in words:
+        reversed_word = word[::-1]
+        reversed_words.append(reversed_word)
+
+    return ' '.join(reversed_words)
