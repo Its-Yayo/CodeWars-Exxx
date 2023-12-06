ec29d0f9c36c1a831636357a37ce86d726039b06
Its-Yayo <a01754574@itesm.mx>
2023-12-06 12:16:33 -0600

    is_prime.c #4: usages

--- src/Easy/is_prime.c
+++ src/Easy/is_prime.c
@@ -1,6 +1,9 @@
 // is_prime.c
 
 /*
+Instructions:
+Source: https://www.codewars.com/kata/5262119038c0985a5b00029f/c
+
 Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
 
 Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
