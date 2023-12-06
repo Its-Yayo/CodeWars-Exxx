a81591a49da133c44afb73e1e8a6894854088797
Its-Yayo <a01754574@itesm.mx>
2023-12-06 11:08:02 -0600

    expanded_form.py: usages

--- src/Easy/expanded_form.py
+++ src/Easy/expanded_form.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+"""
+Instructions:
+Source: https://www.codewars.com/kata/5842df8ccbd22792a4000245
+
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+    expanded_form(12) # Should return '10 + 2'
+    expanded_form(42) # Should return '40 + 2'
+    expanded_form(70304) # Should return '70000 + 300 + 4'
+
+NOTE: All numbers will be whole numbers greater than 0.
+
+
+
+"""
+
+
+def expanded_form(num: int) -> str:
+    result = str(num)
+    list_result = []
+
+    for i, digit in enumerate(result[::-1]):
+        if digit != '0':
+            list_result.append(digit + '0' * i)
+
+    return ' + '.join(list_result[::-1])
