c36a812f6cd20135bcef42f7b66139d00ffd2ffc
Its-Yayo <a01754574@itesm.mx>
2023-12-12 13:30:20 -0600

    find_missing_letter.py: usages #2

--- src/Easy/find_missing_letter.py
+++ src/Easy/find_missing_letter.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 
+"""
+Instructions:
+Source:
+
+Find the missing letter Write a method that takes an array of consecutive (increasing) letters as input and that
+returns the missing letter in the array.
+
+You will always get a valid array. And it will be always exactly one letter be missing. The length of the array will
+always be at least 2. The array will always contain letters in only one case.
+
+Example:
+
+    ['a','b','c','d','f'] -> 'e'
+    ['O','Q','R','S'] -> 'P'
+
+"""
 
 def find_missing_letter(chars: list[str]) -> str:
     # Init approach will take all the ASCII int values
-    # It will transverse from start to end the sum of all the elements
-    # It will return the substraction of the subtotal and the sum of the array (ints)
+    # It will transverse the array from start to end the sum of all the elements
+    # It will return the subtraction of the subtotal and the sum of the array (ints)
     # Just it takes the first and last element of the array to avoid ASCII range errors
 
     ints = [ord(char) for char in chars]
