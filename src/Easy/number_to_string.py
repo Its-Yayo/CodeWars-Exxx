2e28144a3b94db5293031bca39d009a533fc90ee
Its-Yayo <a01754574@itesm.mx>
2023-12-12 13:55:53 -0600

    number_to_string.py: usages #1

---
+++ src/Easy/number_to_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Instructions:
+Source: https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+    123  --> "123"
+    999  --> "999"
+    -100 --> "-100"
+"""
+
+def number_to_string(num: int) -> str:
+    return str(num)
