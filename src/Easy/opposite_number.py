f3487486d52b7efeec6222ee272cd82fa8c4632f
Its-Yayo <elyayoveloz@gmail.com>
2024-02-08 03:47:47 -0600

    opposite_number init

---
+++ src/Easy/opposite_number.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Instructions:
+Source: https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+    1: -1
+    14: -14
+    -34: 34
+
+"""
+
+def opposite(number: int) -> int:
+    return -number
