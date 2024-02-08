c48939623b43a7520e8cf9b2dc83eda051d60b15
Its-Yayo <elyayoveloz@gmail.com>
2024-02-08 03:41:41 -0600

    update make_upper_case.py

---
+++ src/Easy/make_upper_case.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Instructions:
+Source: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+Write a function which converts the input string to uppercase.
+
+"""
+
+def make_upper_case(s: str) -> str:
+    return s.upper()
